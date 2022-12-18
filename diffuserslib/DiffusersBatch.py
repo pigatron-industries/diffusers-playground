db2ae7bc434bc9416f02199323f62487e4c9b00c
Robert Ellis <rob@pigatron-industries.co.uk>
2022-12-18 14:16:51 +0000

    batch with save button

--- diffuserslib/DiffusersBatch.py
+++ diffuserslib/DiffusersBatch.py
@@ -1,6 +1,11 @@
 import itertools
 import numpy as np
 import random
+import time
+
+from IPython.display import display
+import ipywidgets as widgets
+import functools
 
 
 class BatchArgument:
@@ -48,25 +53,77 @@ class StringListBatchArgument(BatchArgument):
         return self.list
 
 
-def createBatchArguments(argdict):
-    batchargs = {}
-    flatargs = {}
-    batch = []
-    for arg in argdict.keys():
-        if(isinstance(argdict[arg], BatchArgument)):
-            batchargs[arg] = argdict[arg]()
-        else:
-            flatargs[arg] = argdict[arg]
-    
-    keys, values = zip(*batchargs.items())
-    for bundle in itertools.product(*values):
-        args = dict(zip(keys, bundle))
-        for flatargkey in flatargs.keys():
-            if(isinstance(flatargs[flatargkey], Argument)):
-                args[flatargkey] = flatargs[flatargkey]()
+
+class DiffusersBatch:
+
+    def __init__(self, pipeline, argdict, outputdir):
+        self.pipeline = pipeline
+        self.argdict = argdict
+        self.outputdir = outputdir
+        self._createBatchArguments()
+        print(f"Created batch of size {len(self.argsbatch)}")
+
+
+    def _createBatchArguments(self):
+        batchargs = {}
+        flatargs = {}
+        batch = []
+        for arg in self.argdict.keys():
+            if(isinstance(self.argdict[arg], BatchArgument)):
+                batchargs[arg] = self.argdict[arg]()
             else:
-                args[flatargkey] = flatargs[flatargkey]
-        batch.append(args)
+                flatargs[arg] = self.argdict[arg]
+        
+        keys, values = zip(*batchargs.items())
+        for bundle in itertools.product(*values):
+            args = dict(zip(keys, bundle))
+            for flatargkey in flatargs.keys():
+                if(isinstance(flatargs[flatargkey], Argument)):
+                    args[flatargkey] = flatargs[flatargkey]()
+                else:
+                    args[flatargkey] = flatargs[flatargkey]
+            batch.append(args)
 
-    return batch
+        self.argsbatch = batch
+        return batch
 
+
+    def run(self):
+        for i, args in enumerate(self.argsbatch):
+            print(f"Running for arguments: {args}")
+            image, seed = self.pipeline(**args)
+            self.argsbatch[i]["image"] = image
+            self.argsbatch[i]["seed"] = seed
+            self.argsbatch[i]["timestamp"] = int(time.time())
+            self._output(image, seed, i)
+
+
+    def _output(self, image, seed, index):
+        self.argsbatch[index]["image"] = image
+        self.argsbatch[index]["seed"] = seed
+        display(image)
+
+        output = widgets.Output()
+        self.argsbatch[index]["output"] = output
+        saveBtn = widgets.Button(description="Save")
+        saveBtn.on_click(functools.partial(self._saveImage, index))
+        display(saveBtn, output)
+
+
+    def _saveImage(self, index, btn):
+        args = self.argsbatch[index]
+        timestamp = args['timestamp']
+        output = args['output']
+        image = args['image']
+        image_filename = f"{self.outputdir}/txt2img_{timestamp}.png"
+        info_filename = f"{self.outputdir}/txt2img_{timestamp}.txt"
+        image.save(image_filename)
+        self._saveArgs(args, info_filename)
+        output.append_stdout("Saved to: " + image_filename)
+
+    def _saveArgs(self, args, file):
+        with open(file, 'w') as file:
+            for arg in args.keys():
+                value = args[arg]
+                if (isinstance(value, str) or isinstance(value, int) or isinstance(value, float)):
+                    file.write(f"{arg}: {value}\n")
