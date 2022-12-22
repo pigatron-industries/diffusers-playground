063c4f49fd87c5a11d965d001f7b6030f753454b
Robert Ellis <roberte@rightmove.co.uk>
2022-12-22 10:37:16 +0000

    add colab option to setup

--- setup.py
+++ setup.py
@@ -4,11 +4,14 @@ def runcmd(cmd, shell=False):
     print(subprocess.run(cmd, stdout=subprocess.PIPE, shell=shell).stdout.decode('utf-8'))
 
 
-def setup():
+def setup(colab=False):
     runcmd(['mkdir workspace'], True)
     os.chdir("workspace")
     runcmd('pwd')
-    runcmd(['pip', 'install', '-r', '../requirements.txt'])
+    if(colab):
+        runcmd(['pip', 'install', '-r', '../requirements_colab.txt'])
+    else:
+        runcmd(['pip', 'install', '-r', '../requirements.txt'])
     runcmd(['pip', 'install', '-e', 'git+https://github.com/joeyballentine/ESRGAN.git#egg=esrgan'])
     runcmd(['pip', 'install', '-e', 'git+https://github.com/pharmapsychotic/BLIP.git@lib#egg=blip'])
     runcmd(['pip', 'install', '-e', 'git+https://github.com/pharmapsychotic/clip-interrogator.git#egg=clip-interrogator'])
