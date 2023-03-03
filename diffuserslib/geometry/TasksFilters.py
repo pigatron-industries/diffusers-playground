77a9d875dcfffea814e9e5200d22399d5a71ff4e
Robert Ellis <roberte@rightmove.co.uk>
2023-03-03 17:34:29 +0000

    add gaussian noise

--- diffuserslib/geometry/TasksFilters.py
+++ diffuserslib/geometry/TasksFilters.py
@@ -1,7 +1,9 @@
 from .GeometryPipeline import GeometryTask
 from .. import evaluateArguments
+from ..ImageUtils import cv2ToPil, pilToCv2
 
-from PIL import ImageEnhance
+from PIL import ImageEnhance, ImageFilter
+import numpy as np
 
 
 class SaturationTask(GeometryTask):
@@ -14,4 +16,33 @@ class SaturationTask(GeometryTask):
         args = evaluateArguments(self.args, context=context)
         converter = ImageEnhance.Color(context.image)
         context.image = converter.enhance(args["saturation"]+1)
+        return context
+    
+
+class GaussianBlurTask(GeometryTask):
+    def __init__(self, radius = 2):
+        self.args = {
+            "radius": radius
+        }
+
+    def __call__(self, context):
+        args = evaluateArguments(self.args, context=context)
+        context.image = context.image.filter(ImageFilter.GaussianBlur(args["radius"]))
+        return context
+    
+class GaussianNoiseTask(GeometryTask):
+    def __init__(self, sigma = 2):
+        self.args = {
+            "sigma": sigma
+        }
+
+    def __call__(self, context):
+        args = evaluateArguments(self.args, context=context)
+
+        image = pilToCv2(context.image)
+        row, col, ch = image.shape
+        gauss = np.random.normal(0, args["sigma"], (row, col, ch))
+        noisy_image = image + gauss
+        noisy_image = np.clip(noisy_image, 0, 255).astype(np.uint8)
+        context.image = cv2ToPil(noisy_image)
         return context
