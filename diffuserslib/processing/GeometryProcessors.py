015b91c2d1a9bdb1824829b5aeb9c3cc5d189f99
Robert Ellis <rob@pigatron-industries.co.uk>
2023-03-09 08:55:14 +0000

    init commit of geomertic spiral

--- diffuserslib/processing/GeometryProcessors.py
+++ diffuserslib/processing/GeometryProcessors.py
@@ -1,4 +1,4 @@
-from .ImageProcessor import ImageProcessor
+from .ImageProcessor import ImageProcessor, ImageContext
 from ..batch import RandomPositionArgument, evaluateArguments
 
 from PIL import ImageDraw
@@ -45,6 +45,62 @@ class DrawCheckerboardProcessor(ImageProcessor):
                     if (i//square_width + j//square_height) % 2 == 1:
                         draw.rectangle([i+context.offset[0], j+context.offset[1], i+square_width+context.offset[0], j+square_height+context.offset[1]], fill=args["fill"])
         return context
+
+
+class DrawGeometricSpiralProcessor(ImageProcessor):
+    def __init__(self, startx = 0, starty = 0, endx = None, endy = None, outline="white", fill="black", ratio = 1/1.618033988749895, iterations=7, direction="right"):
+        self.args = {
+            "startx": startx,
+            "starty": starty,
+            "endx": endx,
+            "endy": endy,
+            "fill": fill,
+            "outline": outline,
+            "ratio": ratio,
+            "iterations": iterations,
+            "direction": direction
+        }
+
+    def __call__(self, context:ImageContext):
+        args = evaluateArguments(self.args, context=context)
+        ratio = args["ratio"]
+        startx = args["startx"] + context.offset[0]
+        starty = args["starty"] + context.offset[1]
+        endx = args["endx"]
+        endy = args["endy"]
+        if endx is None:
+            endx = context.viewport[2]-1
+        else:
+            endx = endx + context.offset[0]
+        if endy is None:
+            endy = context.viewport[3]-1
+        else:
+            endy = endy + context.offset[1]
+        draw = ImageDraw.Draw(context.image)
+
+        rect = [startx, starty, endx, endy]
+        draw.rectangle(rect, fill=args["fill"], outline=args["outline"])
+
+        direction = args["direction"]
+        for i in range(args["iterations"]):
+            rect_one, rect_two = self.splitRectangle(rect, ratio, direction)
+            draw.rectangle(rect_one, fill=args["fill"], outline=args["outline"])
+            
+        return context
+    
+    
+    def splitRectangle(self, rect, ratio, direction):
+        if(direction == "right"):
+            rect_left_width = (rect[2]-rect[0]) * ratio
+            rect_left = [rect[0], rect[1], rect[0]+rect_left_width, rect[3]]
+            rect_right = [rect_left[2], rect[1], rect[2], rect[3]]
+            return rect_left, rect_right
+        if(direction == "left"):
+            rect_left_width = (rect[2]-rect[0]) * (1-ratio)
+            rect_left = [rect[0], rect[1], rect[0]+rect_left_width, rect[3]]
+            rect_right = [rect_left[2], rect[1], rect[2], rect[3]]
+            return rect_right, rect_left
+
 
 
 class DrawJuliaSetProcessor(ImageProcessor):
