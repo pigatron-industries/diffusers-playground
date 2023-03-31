3dc8b8c6b28cad7ef8d0082605729dec478f3dfe
Robert Ellis <roberte@rightmove.co.uk>
2023-03-31 10:29:21 +0100

    move notebooks to examples

---
+++ notebooks/examples/path.py
@@ -0,0 +1,8 @@
+import sys
+
+def setPathLocalNotebook():
+    sys.path.append('../..')
+    sys.path.append('../../workspace/src/esrgan')
+    sys.path.append('../../workspace/src/blip')
+    sys.path.append('../../workspace/src/clip-interrogator')
+    sys.path.append('../../workspace/src/nafnet')
--- notebooks/path.py
+++ notebooks/path.py
@@ -1,4 +1,4 @@
-import os, sys
+import sys
 
 def setPathLocalNotebook():
     sys.path.append('..')
