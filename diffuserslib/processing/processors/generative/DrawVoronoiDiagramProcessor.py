38371dfd55a1d9ac07b0f41209b72c54799fc121
Robert Ellis <rob@pigatron-industries.co.uk>
2023-05-20 21:58:23 +0100

    add edge favouring power distribution

--- diffuserslib/processing/processors/generative/DrawVoronoiDiagramProcessor.py
+++ diffuserslib/processing/processors/generative/DrawVoronoiDiagramProcessor.py
@@ -6,19 +6,30 @@ from typing import List, Tuple, Callable
 from scipy.spatial import Voronoi
 import math
 import numpy as np
+import itertools
+
+
+def edge_power_distribution(a, size):
+    power_distribution = np.random.power(a=a, size=size)
+    for idx in itertools.product(*[range(s) for s in size]):
+        if (np.random.random() < 0.5):
+            power_distribution[idx] = 1 - power_distribution[idx]
+    return power_distribution
 
 
 class DrawVoronoiDiagramProcessor(ImageProcessor):
     def __init__(self, 
                  points: List[Tuple[float, float]] | Callable[[], List[Tuple[float, float]]],
                  outline: str | Callable[[], str] = "white", 
-                 draw: Tuple[bool, bool] | Callable[[], Tuple[bool, bool]] = (True, True),  # (lines, points)
+                 draw: Tuple[bool, bool, bool] | Callable[[], Tuple[bool, bool, bool]] = (True, True, True),  # (bounded lines, unbounded lines, points)
+                 lineProbablity: float | Callable[[], float] = 1,
                  radius: float = 2):
         self.args = {
             "points": points,
             "outline": outline,
             "draw": draw,
-            "radius": radius
+            "radius": radius,
+            "lineProbablity": lineProbablity
         }
 
     def __call__(self, context:ImageContext):
@@ -28,16 +39,18 @@ class DrawVoronoiDiagramProcessor(ImageProcessor):
         args = self.evaluateArguments(context=context)
         points = np.array(args["points"])
         outline = args["outline"]
-        drawLines = args["draw"][0]
-        drawPoints = args["draw"][1]
+        drawBoundedLines = args["draw"][0]
+        drawUnboundedLines = args["draw"][1]
+        drawPoints = args["draw"][2]
         radius = args["radius"]
+        lineProbablity = args["lineProbablity"]
 
         points = points * context.size
-        lines = self.getLines(points)
+        lines = self.getLines(points, boundedLines=drawBoundedLines, unboundedLines=drawUnboundedLines)
         draw = ImageDraw.Draw(context.image)
 
-        if (drawLines):
-            for line in lines:
+        for line in lines:
+            if (np.random.random() < lineProbablity):
                 draw.line(line, fill=outline, width=1)
 
         if (drawPoints):
@@ -47,19 +60,19 @@ class DrawVoronoiDiagramProcessor(ImageProcessor):
         return context
     
 
-    def getLines(self, points) -> List[Tuple[float, float, float, float]]:
+    def getLines(self, points, boundedLines:bool = True, unboundedLines:bool = True) -> List[Tuple[float, float, float, float]]:
         voronoi = Voronoi(points)
         centre = voronoi.points.mean(axis=0)
         lines = []
 
         for ridge_points, ridge_vertices in voronoi.ridge_dict.items():
-            if -1 not in ridge_vertices:
+            if boundedLines and -1 not in ridge_vertices:
                 # finite ridge between two vertices
                 vert1 = voronoi.vertices[ridge_vertices[0]]
                 vert2 = voronoi.vertices[ridge_vertices[1]]
                 line = (vert1[0], vert1[1], vert2[0], vert2[1])
                 lines.append(line)
-            else:
+            elif unboundedLines:
                 # infinite ridge
                 vert = voronoi.vertices[ridge_vertices[1]]
                 point1 = voronoi.points[ridge_points[0]]
