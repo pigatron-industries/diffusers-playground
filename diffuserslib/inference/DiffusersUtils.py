eb393ca6c607605be0fe37cdf288194e1f6bb3df
Robert Ellis <rob@pigatron-industries.co.uk>
2023-06-24 15:41:24 +0100

    check controlmodel array length

--- diffuserslib/inference/DiffusersUtils.py
+++ diffuserslib/inference/DiffusersUtils.py
@@ -80,7 +80,7 @@ def tiledProcessorCentred(tileprocessor, initimage, tilewidth=640, tileheight=64
 
 def compositedInpaint(pipelines:DiffusersPipelines, initimage, maskimage, prompt, negprompt, scale, steps=50, strength=1.0, scheduler=None, seed=None, maskDilation=21, maskFeather=3, model=None, controlmodel=None, controlimage=None):
     """ Standard inpaint but the result is composited back to the original using a feathered mask """
-    if(controlmodel is None):
+    if(controlmodel is None or len(controlmodel) == 0):
         outimage, usedseed = pipelines.inpaint(initimage=initimage, maskimage=maskimage, prompt=prompt, negprompt=negprompt, steps=steps, scale=scale, strength=strength, scheduler=scheduler, seed=seed, model=model)
     else:
         outimage, usedseed = pipelines.inpaintControlNet(initimage=initimage, maskimage=maskimage, prompt=prompt, negprompt=negprompt, steps=steps, scale=scale, scheduler=scheduler, seed=seed, model=model, 
