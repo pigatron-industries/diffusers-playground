68ade5014464340ae326d6a8a774540d489c0f46
Robert Ellis <rob@pigatron-industries.co.uk>
2023-07-09 11:33:44 +0100

    sdxl

---
+++ diffuserslib/inference/arch/StableDiffusionXLPipelines.py
@@ -0,0 +1,29 @@
+from .StableDiffusionPipelines import StableDiffusionPipelineWrapper
+from ...models.DiffusersModelPresets import DiffusersModel
+from ...StringUtils import mergeDicts
+from PIL import Image
+from diffusers import ( # Pipelines
+                        DiffusionPipeline, StableDiffusionXLPipeline, StableDiffusionXLImg2ImgPipeline,
+                        # Schedulers
+                        DDIMScheduler, DDPMScheduler, DPMSolverMultistepScheduler, HeunDiscreteScheduler,
+                        KDPM2DiscreteScheduler, KarrasVeScheduler, LMSDiscreteScheduler, EulerDiscreteScheduler,
+                        KDPM2AncestralDiscreteScheduler, EulerAncestralDiscreteScheduler,
+                        ScoreSdeVeScheduler, IPNDMScheduler, UniPCMultistepScheduler)
+
+
+
+class StableDiffusionXLTextToImagePipelineWrapper(StableDiffusionPipelineWrapper):
+    def __init__(self, preset:DiffusersModel, device, safety_checker=True, **kwargs):
+        super().__init__(StableDiffusionXLPipeline, preset, device, safety_checker=safety_checker)
+
+    def inference(self, prompt, negprompt, width, height, seed, scale, steps, scheduler, **kwargs):
+        return super().inference(prompt=prompt, negative_prompt=negprompt, width=width, height=height, seed=seed, guidance_scale=scale, num_inference_steps=steps, scheduler=scheduler, **args)
+
+
+class StableDiffusionXLImageToImagePipelineWrapper(StableDiffusionPipelineWrapper):
+    def __init__(self, preset:DiffusersModel, device, safety_checker=True, **kwargs):
+        super().__init__(StableDiffusionXLImg2ImgPipeline, preset, device, safety_checker=safety_checker)
+
+    def inference(self, prompt, negprompt, seed, initimage, scale, scheduler, strength, **kwargs):
+        initimage = initimage.convert("RGB")
+        return super().inference(prompt=prompt, negative_prompt=negprompt, seed=seed, image=initimage, guidance_scale=scale, strength=strength, scheduler=scheduler)
