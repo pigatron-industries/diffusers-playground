05356b14bea76cf5c436c1845364b3e4f37c2350
Robert Ellis <rob@pigatron-industries.co.uk>
2024-02-04 16:39:22 +0000

    set conditioning image type

--- diffuserslib/functional/diffusers/ConditioningInputNode.py
+++ diffuserslib/functional/diffusers/ConditioningInputNode.py
@@ -1,6 +1,6 @@
 from ..FunctionalNode import FunctionalNode
 from ..FunctionalTyping import *
-from ...inference.GenerationParameters import ControlImageParameters
+from ...inference.GenerationParameters import ControlImageParameters, ControlImageType
 from PIL import Image
 
 ConditioningInputType = ControlImageParameters
@@ -23,6 +23,6 @@ class ConditioningInputNode(FunctionalNode):
                 image: Image.Image,
                 model: str,
                 scale: float = 1.0) -> ConditioningInputType:
-        conditioning_input = ConditioningInputType(image=image,model=model,condscale=scale)
+        conditioning_input = ConditioningInputType(image = image, type = model, model = model, condscale = scale)
         return conditioning_input
     
