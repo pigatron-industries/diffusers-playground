759f8faff17d8e6c5c245957365c646a7635e5a2
Robert Ellis <rob@pigatron-industries.co.uk>
2024-02-13 08:27:57 +0000

    add parameters

--- diffuserslib/functional_workflows/GenRandomVoronoiNode.py
+++ diffuserslib/functional_workflows/GenRandomVoronoiNode.py
@@ -6,9 +6,10 @@ def name():
 def build():
     size_user_input = SizeUserInputNode(value = (512, 512), name = "size")
     num_points_input = IntUserInputNode(value = 20, name = "num_points")
+    # TODO add more user inputs
 
     random_points = RandomPoints2DNode(num_points = num_points_input)
     new_image = NewImageNode(size = size_user_input, background_colour = (0, 0, 0))
-    voronoi = DrawVoronoiNode(image = new_image, points = random_points)
+    voronoi = DrawVoronoiNode(image = new_image, points = random_points, radius = 2, line_probablity = ProbabilityType(1.0), draw_options = (True, True, True))
 
     return voronoi
