d7d243a88377ee84838817c94b3fc0b1e28cd3b1
Robert Ellis <rob@pigatron-industries.co.uk>
2024-02-14 21:02:18 +0000

    add models path

--- diffuserslib/init.py
+++ diffuserslib/init.py
@@ -10,15 +10,17 @@ class GlobalConfig:
 def initializeDiffusers(configs:List[str]=["config.yml"], modelconfigs:List[str]=["modelconfig.yml"]):
     safety_checker = True
     device = "cuda"
+    models = "./models"
     for config in configs:
         if os.path.exists(config):
             configdata = yaml.safe_load(open(config, "r"))
             device = configdata["device"]
             safety_checker = configdata["safety"]
+            models = configdata["folders"]["models"]
             GlobalConfig.outputs_dir = configdata["folders"]["outputs"]
             break
 
-    DiffusersPipelines.pipelines = DiffusersPipelines(device=device, safety_checker=safety_checker)
+    DiffusersPipelines.pipelines = DiffusersPipelines(device=device, safety_checker=safety_checker, localmodelpath=models)
     for modelconfig in modelconfigs:
         if os.path.exists(modelconfig):
             DiffusersPipelines.pipelines.loadPresetFile(modelconfig)
@@ -28,7 +30,6 @@ def initializeDiffusers(configs:List[str]=["config.yml"], modelconfigs:List[str]
             configdata = yaml.safe_load(open(config, "r"))
             embeddings = configdata["folders"]["embeddings"]
             loras = configdata["folders"]["loras"]
-            models = configdata["folders"]["models"]
             DiffusersPipelines.pipelines.loadTextEmbeddings(embeddings)
             DiffusersPipelines.pipelines.loadLORAs(loras)
 
