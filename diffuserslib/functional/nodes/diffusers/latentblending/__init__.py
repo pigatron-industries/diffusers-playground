c5327ed5c6902beea24e4dd1aa7039693c6f19c8
Robert Ellis <rob@pigatron-industries.co.uk>
2024-02-29 17:38:08 +0000

    remove import

--- diffuserslib/functional/nodes/diffusers/latentblending/__init__.py
+++ diffuserslib/functional/nodes/diffusers/latentblending/__init__.py
@@ -1,3 +1,3 @@
 from .blending_engine import BlendingEngine
 from .diffusers_holder import DiffusersHolder
-from .utils import interpolate_spherical, add_frames_linear_interp, interpolate_linear, get_spacing, get_time, yml_load, yml_save
+from .utils import interpolate_spherical, interpolate_linear, get_spacing, get_time, yml_load, yml_save
