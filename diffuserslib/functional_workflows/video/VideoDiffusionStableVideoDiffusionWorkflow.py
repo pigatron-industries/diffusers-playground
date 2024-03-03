f9b41351430b35a5261534f760c66c6515e567d7
Robert Ellis <rob@pigatron-industries.co.uk>
2024-03-03 13:30:33 +0000

    svd parameters

--- diffuserslib/functional_workflows/video/VideoDiffusionStableVideoDiffusionWorkflow.py
+++ diffuserslib/functional_workflows/video/VideoDiffusionStableVideoDiffusionWorkflow.py
@@ -10,10 +10,8 @@ class VideoDiffusionStableVideoDiffussionWorkflow(WorkflowBuilder):
         model_input = DiffusionModelUserInputNode(basemodels=["svd_1_0"], name="model")
         size_input = SizeUserInputNode(value = (512, 512), name="size")
         seed_input = SeedUserInputNode(value = None, name="seed")
-        steps_input = IntUserInputNode(value = 20, name = "steps")
-        cfgscale_input = FloatUserInputNode(value = 7.0, name = "cfgscale")
-        scheduler_input = ListSelectUserInputNode(value = "DPMSolverMultistepScheduler", options = ImageDiffusionNode.SCHEDULERS, name="scheduler")
         frames_input = IntUserInputNode(value = 16, name = "frames")
+        fps_input = IntUserInputNode(value = 7, name = "fps")
 
         image_input = ImageSelectInputNode(name = "image")
 
@@ -23,12 +21,9 @@ class VideoDiffusionStableVideoDiffussionWorkflow(WorkflowBuilder):
         animatediff = VideoDiffusionStableVideoDiffusionNode(models = model_input, 
                                                             size = size_input, 
                                                             seed = seed_input,
-                                                            steps = steps_input,
-                                                            cfgscale = cfgscale_input,
-                                                            scheduler = scheduler_input,
                                                             frames = frames_input,
                                                             conditioning_inputs = conditioning_inputs)
-        frames_to_video = FramesToVideoNode(frames = animatediff, fps = 7.5)
+        frames_to_video = FramesToVideoNode(frames = animatediff, fps = fps_input)
         
         # model_input.addUpdateListener(lambda: prompt_processor.setWildcardDict(DiffusersPipelines.pipelines.getEmbeddingTokens(models_input.basemodel)))
         return frames_to_video
--- diffuserslib/inference/arch/StableVideoDiffusionPipelines.py
+++ diffuserslib/inference/arch/StableVideoDiffusionPipelines.py
@@ -19,7 +19,7 @@ class StableVideoDiffusionPipelineWrapper(StableDiffusionPipelineWrapper):
         generator, seed = self.createGenerator(seed)
         if(scheduler is not None):
             self.loadScheduler(scheduler)
-        output = self.pipeline(image, decode_chunk_size = 8, generator=generator, **kwargs)
+        output = self.pipeline(image, generator=generator, **kwargs)
         return output, seed
 
 
@@ -34,6 +34,15 @@ class StableVideoDiffusionGeneratePipelineWrapper(StableVideoDiffusionPipelineWr
         if(initimageparams is None or initimageparams.image is None):
             raise ValueError("No init image specified")
         diffusers_params['image'] = initimageparams.image.convert("RGB")
+        diffusers_params['width'] = initimageparams.image.width
+        diffusers_params['height'] = initimageparams.image.height
         diffusers_params['seed'] = params.seed
+        diffusers_params['num_frames'] = params.frames
+        diffusers_params['fps'] = 7
+        diffusers_params['min_guidance_scale'] = 1.0
+        diffusers_params['max_guidance_scale'] = 3.0
+        diffusers_params['motion_bucket_id'] = 127
+        diffusers_params['noise_aug_strength'] = 0.02
+        diffusers_params['decode_chunk_size'] = 8
         output, seed = super().diffusers_inference(**diffusers_params)
         return output.frames[0], seed
