3188be5c13f39352af2075dcc2db400b9531ddf5
Robert Ellis <rob@pigatron-industries.co.uk>
2024-03-03 16:59:38 +0000

    tidy up

--- diffuserslib/functional_workflows/ImageDiffusionConditioningWorkflow.py
+++ diffuserslib/functional_workflows/ImageDiffusionConditioningWorkflow.py
@@ -20,18 +20,18 @@ class ImageDiffusionConditioningWorkflow(WorkflowBuilder):
                                                 name = "scheduler",
                                                 options = ImageDiffusionNode.SCHEDULERS)
         
+        model_input.addUpdateListener(lambda: prompt_processor.setWildcardDict(DiffusersPipelines.pipelines.getEmbeddingTokens(model_input.basemodel)))
+        prompt_processor = RandomPromptProcessorNode(prompt = prompt_input, name = "prompt_processor")
+
         def create_conditioning_input():
             conditioning_model_input = ConditioningModelUserInputNode(diffusion_model_input = model_input, name = "model")
             scale_input = FloatUserInputNode(value = 1.0, name = "scale")
             image_input = ImageSelectInputNode(name = "image")
             resize_type_input = EnumSelectUserInputNode(value = ResizeImageNode.ResizeType.EXTEND, enum = ResizeImageNode.ResizeType, name = "resize_type")
-            # TODO prevent size fields showing twice if they are linked to 2 different nodes
             resize_image = ResizeImageNode(image = image_input, size = size_input, type = resize_type_input, name = "resize_image")
             return ConditioningInputNode(image = resize_image, model = conditioning_model_input, scale = scale_input, name = "conditioning_input")
 
         conditioning_inputs = ListUserInputNode(input_node_generator = create_conditioning_input, name = "conditioning_inputs")
-        prompt_processor = RandomPromptProcessorNode(prompt = prompt_input, name = "prompt_processor")
-
         diffusion = ImageDiffusionNode(models = model_input,
                                     loras = lora_input,
                                     size = size_input,
@@ -42,6 +42,4 @@ class ImageDiffusionConditioningWorkflow(WorkflowBuilder):
                                     seed = seed_input,
                                     scheduler = scheduler_input,
                                     conditioning_inputs = conditioning_inputs)
-        
-        model_input.addUpdateListener(lambda: prompt_processor.setWildcardDict(DiffusersPipelines.pipelines.getEmbeddingTokens(model_input.basemodel)))
         return diffusion
