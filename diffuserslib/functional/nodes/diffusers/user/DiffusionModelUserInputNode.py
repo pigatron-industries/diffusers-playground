49dfb6820f196315df419151fb75a1fc1eb37bea
Robert Ellis <rob@pigatron-industries.co.uk>
2024-03-06 18:31:07 +0000

    layout changes

--- diffuserslib/functional/nodes/diffusers/user/DiffusionModelUserInputNode.py
+++ diffuserslib/functional/nodes/diffusers/user/DiffusionModelUserInputNode.py
@@ -52,12 +52,14 @@ class DiffusionModelUserInputNode(UserInputNode):
             raise Exception("DiffusersPipelines not initialised") 
         with ui.dialog(value = True):
             with ui.card().classes('grow').style('width: 100%; height: 100%'):
-                with ui.column():
+                with ui.column().classes('grow'):
                     ui.label("Embeddings")
                     embeddings = DiffusersPipelines.pipelines.getEmbeddingTokens(self.basemodel)
-                    with ui.scroll_area().classes('w-32 h-32 border grow'):
-                        for embedding in embeddings:
-                            ui.label(embedding)
+                    with ui.scroll_area().classes('w-32 h-32 border grow').style('width:300px;'):
+                        with ui.list().props('dense'):
+                            for embedding in embeddings:
+                                with ui.item():
+                                    ui.label(embedding)
 
 
     def updateModels(self):
