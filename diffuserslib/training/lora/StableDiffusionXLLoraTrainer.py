2446b2a1a1c62608e771be3f9e138660ee7eb379
Robert Ellis <rob@pigatron-industries.co.uk>
2024-03-09 12:02:32 +0000

    mkae target size a tuple

--- diffuserslib/training/lora/StableDiffusionXLLoraTrainer.py
+++ diffuserslib/training/lora/StableDiffusionXLLoraTrainer.py
@@ -390,7 +390,7 @@ class StableDiffusionXLLoraTrainer(DiffusersTrainer):
 
     def compute_time_ids(self, original_size, crops_coords_top_left):
         # Adapted from pipeline.StableDiffusionXLPipeline._get_add_time_ids
-        target_size = self.params.resolution
+        target_size = (self.params.resolution, self.params.resolution)
         add_time_ids = list(original_size + crops_coords_top_left + target_size)
         add_time_ids = torch.tensor([add_time_ids])
         add_time_ids = add_time_ids.to(self.accelerator.device, dtype=self.weight_dtype)
