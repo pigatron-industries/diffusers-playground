b6f5a3694f095d3963b0b9f19c719045628a5702
Robert Ellis <rob@pigatron-industries.co.uk>
2024-03-09 22:37:29 +0000

    add lora weight input

--- diffuserslib/functional/nodes/diffusers/user/LORAModelUserInputNode.py
+++ diffuserslib/functional/nodes/diffusers/user/LORAModelUserInputNode.py
@@ -12,17 +12,23 @@ class LORAModelUserInputNode(UserInputNode):
 
     def __init__(self, diffusion_model_input:DiffusionModelUserInputNode, name:str="lora_model_user_input"):
         self.lora = None
+        self.weight = 1.0
         self.diffusion_model_input = diffusion_model_input
         diffusion_model_input.addUpdateListener(self.updateModels)
         super().__init__(name)
 
 
-    def getValue(self) -> str|None:
-        return self.lora
+    def getValue(self) -> tuple[str|None, float]|None:
+        return self.lora, self.weight
     
 
-    def setValue(self, value:str|None):
-        self.lora = value
+    def setValue(self, value:tuple[str|None, float]|None):
+        if value is not None:
+            self.lora = value[0]
+            self.weight = value[1]
+        else:
+            self.lora = None
+            self.weight = 1.0
 
 
     @ui.refreshable
@@ -33,6 +39,7 @@ class LORAModelUserInputNode(UserInputNode):
         if (self.lora is not None and self.lora not in loras):
             self.lora = None
         self.lora_dropdown = ui.select(options=[None]+list(loras), value=self.lora, label="Lora").bind_value(self, 'lora').classes('grow')
+        self.weight_dropdown = ui.number(value=1.0, label="Weight").bind_value(self, 'weight')
 
 
     def updateModels(self):
@@ -42,5 +49,5 @@ class LORAModelUserInputNode(UserInputNode):
     def process(self) -> LorasType:
         loraparams:List[LoraParameters] = []
         if self.lora is not None:
-            loraparams.append(LoraParameters(self.lora, 1.0))
+            loraparams.append(LoraParameters(self.lora, self.weight))
         return loraparams
