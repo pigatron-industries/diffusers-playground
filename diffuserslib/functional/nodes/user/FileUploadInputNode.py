a75d0cb166fda675ff5e3c92b77f77ff575e4198
Robert Ellis <rob@pigatron-industries.co.uk>
2024-04-21 12:32:00 +0100

    async

--- diffuserslib/functional/nodes/user/FileUploadInputNode.py
+++ diffuserslib/functional/nodes/user/FileUploadInputNode.py
@@ -42,7 +42,7 @@ class FileUploadInputNode(UserInputNode):
         pass
 
 
-    def paste(self):
+    async def paste(self):
         raise NotImplementedError("Clipboard paste not implemented")
 
     
@@ -72,7 +72,7 @@ class ImageUploadInputNode(FileUploadInputNode):
             ui.label(f'{self.content.width} x {self.content.height} pixels')
         return container
     
-    def paste(self):
+    async def paste(self):
         content = Clipboard.readObject()
         if(content is not None):
             self.content = content
