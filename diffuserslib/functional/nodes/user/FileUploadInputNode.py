40ab152fd8a203b55a92d8262fbcbb8410ea3a95
Robert Ellis <rob@pigatron-industries.co.uk>
2024-04-21 12:35:43 +0100

    io_bound

--- diffuserslib/functional/nodes/user/FileUploadInputNode.py
+++ diffuserslib/functional/nodes/user/FileUploadInputNode.py
@@ -2,7 +2,7 @@ from diffuserslib.functional.FunctionalNode import *
 from diffuserslib.functional.types.FunctionalTyping import *
 from diffuserslib.interface.Clipboard import Clipboard
 from .UserInputNode import UserInputNode
-from nicegui import ui, events
+from nicegui import ui, events, run
 from PIL import Image
 
 
@@ -35,7 +35,7 @@ class FileUploadInputNode(UserInputNode):
             else:
                 self.previewContent()
             ui.button(icon='folder', on_click=dialog.open).props('dense')
-            ui.button(icon='content_paste', on_click=self.paste).props('dense')
+            ui.button(icon='content_paste', on_click=lambda: run.io_bound(self.paste)).props('dense')
 
 
     def previewContent(self):
