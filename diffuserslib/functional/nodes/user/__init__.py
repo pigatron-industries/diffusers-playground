11c10223796af07c6ee4a13394f8a86bc66abf81
Robert Ellis <rob@pigatron-industries.co.uk>
2024-05-05 11:28:01 +0100

    add import

--- diffuserslib/functional/nodes/user/__init__.py
+++ diffuserslib/functional/nodes/user/__init__.py
@@ -2,4 +2,5 @@ from .UserInputNode import *
 from .ListUserInputNode import *
 from .FileUploadInputNode import *
 from .FileSelectInputNode import *
+from .ImageUploadInputNode import *
 from .VideoUploadInputNode import *
