a3e9a661c28ed86a63bc35c984be2b4db5a5f230
Robert Ellis <rob@pigatron-industries.co.uk>
2024-06-12 18:30:23 +0100

    add sd 3

---
+++ diffuserslib/inference/arch/StableDiffusion3Pipelines.py
@@ -0,0 +1,97 @@
+from .StableDiffusionPipelines import StableDiffusionPipelineWrapper
+from ..GenerationParameters import GenerationParameters, ControlImageType
+from diffuserslib.models.DiffusersModelPresets import DiffusersModelType
+from typing import List
+from PIL import Image
+from diffusers import ( # Pipelines
+                        StableDiffusion3Pipeline, StableDiffusion3Img2ImgPipeline,
+                        # Conditioning models
+                        T2IAdapter, ControlNetModel,
+                        # Schedulers
+                        DDIMScheduler, DDPMScheduler, DPMSolverMultistepScheduler, DPMSolverSinglestepScheduler, HeunDiscreteScheduler,
+                        KDPM2DiscreteScheduler, KarrasVeScheduler, LMSDiscreteScheduler, EulerDiscreteScheduler,
+                        KDPM2AncestralDiscreteScheduler, EulerAncestralDiscreteScheduler,
+                        ScoreSdeVeScheduler, IPNDMScheduler, UniPCMultistepScheduler, LCMScheduler, DPMSolverSDEScheduler)
+import torch
+
+
+class StableDiffusion3PipelineWrapper(StableDiffusionPipelineWrapper):
+
+    def __init__(self, cls, params:GenerationParameters, device, dtype=None):
+        super().__init__(cls=cls, params=params, device=device, dtype=dtype)
+                
+
+    def diffusers_inference(self, prompt, negative_prompt, seed, scheduler=None, tiling=False, **kwargs):
+        generator, seed = self.createGenerator(seed)
+        if(scheduler is not None):
+            self.loadScheduler(scheduler)
+        self.pipeline.vae.enable_tiling(tiling)
+
+        output = self.pipeline(prompt = prompt,
+                              negative_prompt = negative_prompt,
+                              generator=generator, **kwargs)
+        return output, seed
+
+
+
+class StableDiffusion3GeneratePipelineWrapper(StableDiffusion3PipelineWrapper):
+
+    PIPELINE_MAP = {
+        #img2im,    controlnet, t2iadapter, inpaint
+        (False,     False,      False,      False):    StableDiffusion3Pipeline,
+        # (False,     True,       False,      False):    StableDiffusion3ControlNetPipeline,
+        # (False,     False,      True,       False):    StableDiffusion3AdapterPipeline,
+        (True,      False,      False,      False):    StableDiffusion3Img2ImgPipeline,
+        # (True,      True,       False,      False):    StableDiffusion3ControlNetImg2ImgPipeline,
+        # (True,      False,      False,      True):     StableDiffusion3InpaintPipeline,
+        # (True,      True,       False,      True):     StableDiffusion3ControlNetInpaintPipeline,
+    }
+
+
+    def __init__(self, params:GenerationParameters, device):
+        self.dtype = None
+        self.features = self.getPipelineFeatures(params)
+        cls = self.getPipelineClass(params)
+
+        super().__init__(params=params, device=device, cls=cls)
+
+        if(self.features.ipadapter):
+            self.initIpAdapter(params)
+
+
+    def getPipelineClass(self, params:GenerationParameters):
+        self.features = self.getPipelineFeatures(params)
+        return self.PIPELINE_MAP[(self.features.img2img, self.features.controlnet, self.features.t2iadapter, self.features.inpaint)]  
+
+
+    def createPipelineParams(self, params:GenerationParameters):
+        pipeline_params = {}
+        self.addPipelineParamsCommon(params, pipeline_params)
+        if(self.features.controlnet):
+            self.addPipelineParamsControlNet(params, pipeline_params)
+        if(self.features.t2iadapter):
+            self.addPipelineParamsT2IAdapter(params, pipeline_params)
+        if(self.features.ipadapter):
+            self.addPipelineParamsIpAdapter(params, pipeline_params)
+        return pipeline_params
+
+
+    def inference(self, params:GenerationParameters):
+        diffusers_params = {}
+        self.addInferenceParamsCommon(params, diffusers_params)
+        if(not self.features.img2img):
+            self.addInferenceParamsTxt2Img(params, diffusers_params)
+        if(self.features.img2img):
+            self.addInferenceParamsImg2Img(params, diffusers_params)
+        if(self.features.differential):
+            self.addInferenceParamsDifferential(params, diffusers_params)
+        if(self.features.controlnet):
+            self.addInferenceParamsControlNet(params, diffusers_params)
+        if(self.features.t2iadapter):
+            self.addInferenceParamsT2IAdapter(params, diffusers_params)
+        if(self.features.ipadapter):
+            self.addInferenceParamsIpAdapter(params, diffusers_params)
+        if(self.features.inpaint):
+            self.addInferenceParamsInpaint(params, diffusers_params)
+        output, seed = super().diffusers_inference(**diffusers_params)
+        return output.images[0], seed
--- diffuserslib/inference/arch/__init__.py
+++ diffuserslib/inference/arch/__init__.py
@@ -1,6 +1,7 @@
 from .DiffusersPipelineWrapper import *
 from .StableDiffusionPipelines import *
 from .StableDiffusionXLPipelines import *
+from .StableDiffusion3Pipelines import *
 from .StableCascadePipelines import *
 from .StableVideoDiffusionPipelines import *
 from .DeepFloydPipelines import *
