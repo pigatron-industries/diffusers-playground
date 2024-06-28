b85958b25d04dfbe3f74d1cba7bd23d3dd602106
Robert Ellis <rob@pigatron-industries.co.uk>
2024-06-28 20:33:52 +0100

    change label

--- diffuserslib/functional/nodes/user/ImageUploadInputNode.py
+++ diffuserslib/functional/nodes/user/ImageUploadInputNode.py
@@ -10,7 +10,7 @@ import tempfile
 class ImageUploadInputNode(FileUploadInputNode):
     """A node that allows the user to upload a single image. The output is an image."""
 
-    def __init__(self, mandatory:bool = True, display:str = "Select File", name:str="image_input"):
+    def __init__(self, mandatory:bool = True, display:str = "Select image file", name:str="image_input"):
         self.preview = None
         super().__init__(mandatory, display, name)
 
