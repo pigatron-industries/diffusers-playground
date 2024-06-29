3df066bbd7184ae4f62fd9142e8515f7919547ec
Robert Ellis <rob@pigatron-industries.co.uk>
2024-06-29 18:41:12 +0100

    add start frame and skip frames parameters

--- diffuserslib/functional/nodes/animated/VideoFrameSplitterNode.py
+++ diffuserslib/functional/nodes/animated/VideoFrameSplitterNode.py
@@ -1,36 +1,43 @@
 from diffuserslib.functional.FunctionalNode import *
-from diffuserslib.functional.types.Video import *
+from diffuserslib.functional.types import *
 import cv2
 
 
 class VideoFrameSplitterNode(FunctionalNode):
     def __init__(self, 
                  video:VideoFuncType,
+                 start_frame:IntFuncType = 0,
+                 skip_frames:IntFuncType = 0,
                  name:str = "frame_splitter"):
         super().__init__(name)
         self.addInitParam("video", video, Video)
+        self.addInitParam("start_frame", start_frame, int)
+        self.addInitParam("skip_frames", skip_frames, int)
         self.framenum = 0
         self.framecount = 0
         self.capture = None
     
 
-    def init(self, video:Video):
+    def init(self, video:Video, start_frame:int = 0, skip_frames:int = 0):
         self.video = video
-        self.framenum = 0
+        self.skip_frames = skip_frames
+        self.framenum = start_frame
         if(self.capture is not None):
             self.capture.release()
         self.capture = cv2.VideoCapture(video.file.name)
         self.framecount = int(self.capture.get(cv2.CAP_PROP_FRAME_COUNT))
+        self.capture.set(cv2.CAP_PROP_POS_FRAMES, self.framenum)
 
 
     def process(self) -> Image.Image|None:
         assert self.capture is not None
         if(self.framenum < self.framecount):
+            self.capture.set(cv2.CAP_PROP_POS_FRAMES, self.framenum)
             ret, frame = self.capture.read()
             if not ret:
                 raise ValueError("Could not read video file")
             frame = Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
-            self.framenum += 1
+            self.framenum += (self.skip_frames + 1)
             return frame
         else:
             return None
--- diffuserslib/functional_workflows/image/partial/FrameSplitterWorkflow.py
+++ diffuserslib/functional_workflows/image/partial/FrameSplitterWorkflow.py
@@ -3,6 +3,7 @@ from diffuserslib.functional.WorkflowBuilder import *
 from diffuserslib.functional.types.FunctionalTyping import *
 from diffuserslib.functional.nodes.animated.VideoFrameSplitterNode import *
 from diffuserslib.functional.nodes.user.VideoUploadInputNode import *
+from diffuserslib.functional.nodes.user.UserInputNode import *
 
 
 class FrameSplitterWorkflow(WorkflowBuilder):
@@ -13,6 +14,8 @@ class FrameSplitterWorkflow(WorkflowBuilder):
 
     def build(self):
         video_input = VideoUploadInputNode(name = "video")
-        frame = VideoFrameSplitterNode(video = video_input)
+        start_frame = IntUserInputNode(value = 0, name = "start_frame")
+        skip_frames = IntUserInputNode(value = 0, name = "skip_frames")
+        frame = VideoFrameSplitterNode(video = video_input, start_frame = start_frame, skip_frames = skip_frames)
         return frame
     
