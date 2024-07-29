538b1f784b3f7a6c7b5b60e650fe3721ad3db76b
Robert Ellis <rob@pigatron-industries.co.uk>
2024-07-29 20:57:38 +0100

    load modules once

--- diffuserslib/functional/nodes/text/ImageToPezNode.py
+++ diffuserslib/functional/nodes/text/ImageToPezNode.py
@@ -22,6 +22,9 @@ class ImageToPezNode(FunctionalNode):
         }
     }
 
+    open_clip = None
+    optim_utils = None
+
 
     def __init__(self, 
                  image:ImageFuncType,
@@ -32,13 +35,15 @@ class ImageToPezNode(FunctionalNode):
         self.addParam("image", image, Image.Image)
         self.addParam("clip_model", clip_model, str)
         self.addParam("iterations", iterations, int)
+        if not ImageToPezNode.open_clip and not ImageToPezNode.optim_utils:
+            ModuleLoader.load_from_directory("workspace/hard-prompts-made-easy/open_clip", recursive=True)
+            ModuleLoader.load_from_file("workspace/hard-prompts-made-easy/optim_utils.py", module_name="optim_utils")
+            ImageToPezNode.open_clip = sys.modules["open_clip"]
+            ImageToPezNode.optim_utils = sys.modules["optim_utils"]
         
 
     def process(self, image:Image.Image, clip_model:str, iterations:int) -> str:
-        ModuleLoader.load_from_directory("workspace/hard-prompts-made-easy/open_clip", recursive=True)
-        ModuleLoader.load_from_file("workspace/hard-prompts-made-easy/optim_utils.py", module_name="optim_utils")
-        open_clip = sys.modules["open_clip"]
-        optim_utils = sys.modules["optim_utils"]
+        assert ImageToPezNode.open_clip and ImageToPezNode.optim_utils, "open_clip and optim_utils modules not loaded"
 
         args = argparse.Namespace()
         args.__dict__["iter"] = iterations
@@ -52,7 +57,7 @@ class ImageToPezNode(FunctionalNode):
         args.__dict__["clip_model"] = self.CLIP_MODELS[clip_model]["model"]
         args.__dict__["clip_pretrain"] = self.CLIP_MODELS[clip_model]["pretrain"]
 
-        model, _, preprocess = open_clip.create_model_and_transforms(args.__dict__["clip_model"], args.__dict__["clip_pretrain"], device="mps")
-        learned_prompt = optim_utils.optimize_prompt(model, preprocess, args, "mps", target_images=[image])
+        model, _, preprocess = ImageToPezNode.open_clip.create_model_and_transforms(args.__dict__["clip_model"], args.__dict__["clip_pretrain"], device="mps")
+        learned_prompt = ImageToPezNode.optim_utils.optimize_prompt(model, preprocess, args, "mps", target_images=[image])
 
         return learned_prompt
