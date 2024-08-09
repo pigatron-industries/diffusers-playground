b8592d78e2df030e896d290847fad63f48459a25
Robert Ellis <rob@pigatron-industries.co.uk>
2024-08-09 20:14:43 +0100

    use diffusers for stable audio

--- diffuserslib/functional/nodes/audio/StableAudioNode.py
+++ diffuserslib/functional/nodes/audio/StableAudioNode.py
@@ -2,18 +2,20 @@ from diffuserslib.functional.types import *
 from diffuserslib.functional.FunctionalNode import *
 from diffuserslib.functional.types.FunctionalTyping import *
 from diffuserslib.GlobalConfig import GlobalConfig
-
+from diffusers import StableAudioPipeline
 from stable_audio_tools import get_pretrained_model
 from stable_audio_tools.inference.generation import generate_diffusion_cond
 from einops import rearrange
 import torch
 import huggingface_hub
 
+MAX_SEED = 4294967295
 
 class StableAudioNode(FunctionalNode):
 
     def __init__(self, 
                  prompt:StringFuncType = "",
+                 negative_prompt:StringFuncType = "",
                  duration:FloatFuncType = 10.0,
                  steps:FloatFuncType = 100,
                  cfg_scale:IntFuncType = 7,
@@ -21,43 +23,25 @@ class StableAudioNode(FunctionalNode):
                  name:str="stableaudio"):
         super().__init__(name)
         self.addParam("prompt", prompt, str)
+        self.addParam("negative_prompt", negative_prompt, str)
         self.addParam("duration", duration, float)
         self.addParam("steps", steps, int)
         self.addParam("cfg_scale", cfg_scale, float)
         self.addParam("seed", seed, int)
         self.model = None
-        self.model_config = None
         
         
-    def process(self, prompt:str, duration:float, steps:int, cfg_scale:float, seed:int):
+    def process(self, prompt:str, negative_prompt:str, duration:float, steps:int, cfg_scale:float, seed:int|None):
         if (self.model is None):
-            self.model, self.model_config = get_pretrained_model("stabilityai/stable-audio-open-1.0")
-            self.model.to(GlobalConfig.device)
+            self.model = StableAudioPipeline.from_pretrained("stabilityai/stable-audio-open-1.0", torch_dtype=torch.float16).to(GlobalConfig.device)
         assert self.model is not None, "Model is not initialized"
-        assert self.model_config is not None, "Model config is not initialized"
-        sample_rate = self.model_config['sample_rate']
-        sample_size = self.model_config['sample_size']
 
-        conditioning = [{
-            "prompt": prompt,
-            "seconds_start": 0,
-            "seconds_total": duration,
-        }]
-
-        output = generate_diffusion_cond(
-            model = self.model,
-            seed = seed,
-            steps = steps,
-            cfg_scale = cfg_scale,
-            conditioning = conditioning,
-            sample_size = sample_size,
-            sigma_min = 0.3,
-            sigma_max = 500,
-            sampler_type = "dpmpp-3m-sde",
-            device = GlobalConfig.device
-        )
-
-        output = rearrange(output, 'b d n -> d (b n)')
-        audio_array = output.to(torch.float32).div(torch.max(torch.abs(output))).clamp(-1, 1).cpu().numpy()
-        audio_array = np.transpose(audio_array, (1, 0))
-        return Audio(audio_array, sample_rate)
+        if(seed is None):
+            seed = random.randint(0, MAX_SEED)
+        generator = torch.Generator("cpu").manual_seed(seed)
+        
+        audio = self.model(prompt, negative_prompt=negative_prompt, num_inference_steps=steps, 
+                           audio_end_in_s=duration, guidance_scale=cfg_scale, generator=generator).audios
+        
+        audio_array = audio[0].T.float().cpu().numpy()
+        return Audio(audio_array, self.model.vae.sampling_rate)
--- diffuserslib/functional_workflows/audio/AudioGenerateStableAudioWorkflow.py
+++ diffuserslib/functional_workflows/audio/AudioGenerateStableAudioWorkflow.py
@@ -12,11 +12,13 @@ class AudioGenerationStableAudioWorkflow(WorkflowBuilder):
     def build(self):
         # files_input = FileSelectInputNode(filetype = "audio", name = "samples")
         prompt_input = TextAreaInputNode(value = "Hello World!", name = "prompt")
+        negative_prompt_input = StringUserInputNode(value = "", name = "negative_prompt")
         duration_input = FloatUserInputNode(value = 10.0, name = "duration")
         steps_input = IntUserInputNode(value = 100, name = "steps")
         cfg_scale_input = FloatUserInputNode(value = 7, name = "cfg_scale")
         seed_input = SeedUserInputNode(value = None, name = "seed")
 
-        audio = StableAudioNode(prompt = prompt_input, duration = duration_input, steps = steps_input, cfg_scale = cfg_scale_input, seed = seed_input)
+        audio = StableAudioNode(prompt = prompt_input, negative_prompt = negative_prompt_input, duration = duration_input, steps = steps_input, 
+                                cfg_scale = cfg_scale_input, seed = seed_input)
         return audio
     
